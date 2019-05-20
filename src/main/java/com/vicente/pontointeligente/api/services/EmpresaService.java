463b174a4ca7a5f84cad25b2323482df9e959812
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-19 21:34:33 -0300

    Criado interfaces para os serviços dos recursos Empresa, Funcionario e Lancamento.

---
+++ src/main/java/com/vicente/pontointeligente/api/services/EmpresaService.java
@@ -0,0 +1,26 @@
+package com.vicente.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import com.vicente.pontointeligente.api.entities.Empresa;
+
+
+public interface EmpresaService {
+
+	/**
+	 * Retorna uma empresa dado um CNPJ.
+	 *
+	 * @param cnpj
+	 * @return Optional<Empresa>
+	 */
+	Optional<Empresa> buscarPorCnpj(String cnpj);
+
+	/**
+	 * Cadastra uma nova empresa na base de dados.
+	 *
+	 * @param empresa
+	 * @return Empresa
+	 */
+	Empresa persistir(Empresa empresa);
+	
+}
---
+++ src/main/java/com/vicente/pontointeligente/api/services/FuncionarioService.java
@@ -0,0 +1,42 @@
+package com.vicente.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import com.vicente.pontointeligente.api.entities.Funcionario;
+
+
+public interface FuncionarioService {
+
+	/**
+	 * Persiste um funcionário na base de dados.
+	 *
+	 * @param funcionario
+	 * @return Funcionario
+	 */
+	Funcionario persistir(Funcionario funcionario);
+
+	/**
+	 * Busca e retorna um funcionário dado um CPF.
+	 *
+	 * @param cpf
+	 * @return Optional<Funcionario>
+	 */
+	Optional<Funcionario> buscarPorCpf(String cpf);
+
+	/**
+	 * Busca e retorna um funcionário dado um email.
+	 *
+	 * @param email
+	 * @return Optional<Funcionario>
+	 */
+	Optional<Funcionario> buscarPorEmail(String email);
+
+	/**
+	 * Busca e retorna um funcionário por ID.
+	 *
+	 * @param id
+	 * @return Optional<Funcionario>
+	 */
+	Optional<Funcionario> buscarPorId(Long id);
+
+}
